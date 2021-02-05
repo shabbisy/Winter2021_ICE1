c5f75a8bde8a3b5c7f88c53282624d6e32a618fb
AJthagod <janeya@sheridancollege.ca>
2021-02-05 17:29:29 -0500

    w3

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -2,16 +2,16 @@ package pickacard;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
+ * @author janeya - 991461495
  * @author srinivsi 
  */
-public class Card {
-
+public class Card 
+{
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
@@ -19,31 +19,37 @@ public class Card {
     /**
      * @return the suit
      */
-    public String getSuit() {
+    public String getSuit() 
+    {
         return suit;
     }
 
     /**
      * @param suit the suit to set
      */
-    public void setSuit(String suit) {
+    public void setSuit(String suit) 
+    {
         this.suit = suit;
     }
 
     /**
      * @return the value
      */
-    public int getValue() {
+    public int getValue() 
+    {
         return value;
     }
 
     /**
      * @param value the value to set
      */
-    public void setValue(int value) {
+    public void setValue(int value) 
+    {
         this.value = value;
-    }
-   
-   
+    } 
     
+    public String toString()
+    {
+        return value + " of " + suit;
+    }
 }
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,62 @@
 package pickacard;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
+ * @author janeya - 991461495
  * @author srinivsi
  */
-public class CardTrick {
-
-    public static void main(String[] args) {
+public class CardTrick 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
         
-        Card[] magicHand = new Card[7];
+        ArrayList<Card> magicHand = new ArrayList<>();
 
-        for (int i = 0; i < magicHand.length; i++) {
+        for(int i = 0; i < 7; i++) //generates 7 random cards
+        {
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random()*12)+1);
+            
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random()*3)]);
+            
+            magicHand.add(c);
         }
-
+        
         //insert code to ask the user for Card value and suit, create their card
+        System.out.print("Please enter a card value and suit separated by a space: ");
+        int    myValue  = input.nextInt();
+        String mySuit   = input.next();
+        
+        Card myCard = new Card();
+        
+        myCard.setValue(myValue);
+        myCard.setSuit(mySuit);
+        
         // and search magicHand here
+        boolean isInHand = false;
+        for(Card searchCard : magicHand)
+        {
         //Then report the result here
+            if(searchCard.equals(myCard))
+                isInHand = true;
+            else
+                isInHand = false;
+        }
+        
+        if(isInHand == true)
+            System.out.println("Your card,  " + myValue + " of " + mySuit
+            + " is in the hand!");
+        else
+            System.out.println("Your card was not in the hand.");
+        
+        System.out.print("The Hand: " + magicHand);
     }
-
 }
