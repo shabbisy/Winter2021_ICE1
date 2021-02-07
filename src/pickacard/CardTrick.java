f50e0f53dd57cd11b1f4f2c0fe386dbb96f744ee
nitinm96 <nitin.minhas@hotmail.com>
2021-02-07 18:46:57 -0500

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
  *
  * @author dancye
  * @author srinivsi
+ * Nitin Minhas
+ * Student # 991381688
  */
 public class CardTrick {
 
@@ -21,35 +23,47 @@ public class CardTrick {
         {
             Card c = new Card();  
             magicHand[i] = c;
-            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);                                    //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);                                        //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             c.setValue((int) (Math.random() * 13 + 1));                                              //c.setValue(insert call to random number generator here)
             System.out.print(c.getSuit());
             System.out.println(", " + c.getValue());  
 
         }
         
-        Scanner input = new Scanner(System.in);                                                 //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner(System.in);                                                       //insert code to ask the user for Card value and suit, create their card
             System.out.println("Choose a Suit for card (Hearts, Diamond, Spades, Clubs:");
             String userSuit = input.next();
             System.out.println("Enter a number to go with your suit from (1-13): ");
             int userValue = input.nextInt();
        
-
-       for(Card c: magicHand)                             //and search magicHand here                          
-        {
-           boolean find = c.getSuit().equals(userSuit) && c.getValue() == userValue;
-            if (find == true)
+            
+       for(Card c: magicHand)                                                                         //and search magicHand here                          
+        {   
+            if (c.getSuit().equals(userSuit) && c.getValue() == userValue)                            //Then report the result here
             {
-                System.out.println("Your card did not match, Please try again...");
-                  break;
+                System.out.println("Congratulations, Your card matched!!");
+                    break;
             }
             else
             {
-                System.out.println("Congratulations, Your card matched!!");
-            }
-                                                                                                 //Then report the result here
-        }
 
+                System.out.println("Your card did not match, Please try again...");
+            }
+                                                                                                 
+        }
+       
+     
+      Card luckyCard = new Card();
+        luckyCard.setSuit(Card.SUITS[2]);
+        luckyCard.setValue(1);
+        for(Card c:magicHand)                                                                          //and search magicHand here                          
+        {
+            if (c.getSuit().equals(luckyCard.getSuit()) && c.getValue() == luckyCard.getValue())
+            {
+                System.out.println("You got the lucky card, You Win!!");
+                  break;
+            }                                                                                          //Then report the result here
+        }
     }
 }
 
