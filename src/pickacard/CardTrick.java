1803d6df5287a30c5583debecae38fa76c813ea1
ZubearNassimi <nassimi@sheridanc.on.ca>
2021-02-07 19:53:18 -0500

    ZubearNassimi

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,27 +1,60 @@
 package pickacard;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author srinivsi
+ * @author Zubear Nassimi
+ * Student # 991 628 529
+ * Class: SYST 17796
+ * Instructor: Sivagama Srinivasan
  */
-public class CardTrick {
 
-    public static void main(String[] args) {
-        
+public class CardTrick 
+{
+    public static void main(String[] args) 
+    {
         Card[] magicHand = new Card[7];
 
-        for (int i = 0; i < magicHand.length; i++) {
+        for (int i = 0; i < magicHand.length; i++) 
+        {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (1+Math.random()*13));
+            c.setSuit(Card.SUITS[(int) (0+Math.random()*3)]);
+            magicHand[i]=c;
         }
-
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
+        for (int i = 0; i < magicHand.length; i++)
+        {
+            System.out.println(magicHand[i].getValue() + " of " + magicHand[i].getSuit());
+        }
+        
+        Scanner userCardValue=new Scanner(System.in);
+        System.out.println("enter a number from 1 - 13 for your card value:");
+        int userValue = userCardValue.nextInt();
+        
+        Scanner userCardSuit=new Scanner(System.in);
+        System.out.println("type Hearts, Diamonds, Spades or Clubs (case-sensitive!):");
+        String userSuit = userCardSuit.nextLine();
+                
+        int count = 0;
+        for (int i=0; i < magicHand.length; i++)
+        {
+            if (magicHand[i].getSuit().equals(userSuit) && userValue == magicHand[i].getValue())
+            {
+                count++;
+            } 
+        }
+        
+        if (count >= 1)
+        {
+            System.out.println("Congrats you got a Match!");
+        }
+        else
+        {
+            System.out.println("Sorry better luck next time");
+        } 
     }
-
 }
