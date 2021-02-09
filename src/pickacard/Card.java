f7d91bf7135e90aab720c2b8bcc2c9a5d219f9e5
Tommy Chiu <toms.cdxvi@gmail.com>
2021-02-09 05:15:36 -0500

    First Commit/Final Commit

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -1,5 +1,7 @@
 package pickacard;
 
+import java.lang.Math;
+
 /**
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
@@ -16,34 +18,32 @@ public class Card {
    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
+    
     public String getSuit() {
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
-     */
     public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
     public void setValue(int value) {
         this.value = value;
     }
    
+    public int randomValue(){
+        int value = (int)(Math.random()*13)+1;
+            return value;
+    }
+    
+    public int randomSuit(){
+        int suit = (int)(Math.random()*4)+0;
+            return suit;
+    }
    
     
 }
