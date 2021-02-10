8f05f36b4bac7ec5dbb7a885519ab281b538ae1e
Akshita <Akshita@DESKTOP-VQDS947>
2021-02-10 10:05:13 -0800

    setValue and setSuit were created. Akshita Sreenivasulu February 8, 2020 991592661

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -22,8 +22,11 @@ public class CardTrick {
             Random rn = new Random();
             int value1 = rn.nextInt(13-1+1)+1;
             int value2 = rn.nextInt(3+1);
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue(value1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[value2]);
         }
 
         //insert code to ask the user for Card value and suit, create their card
