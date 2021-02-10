c8a2c37fb27782dd4af35a20904ff773c5dd3485
Akshita <Akshita@DESKTOP-VQDS947>
2021-02-10 10:47:47 -0800

    Print statements to ask the user to enter a value for suit and a value for num. They will be stored. Akshita Sreenivasulu February 8, 2020 991592661

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -33,6 +33,11 @@ public class CardTrick {
 
         Scanner in = new Scanner(System.in);
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter Card Option - Suit: ");
+        String suit = in.next();
+
+        System.out.println("Enter Card Option - Number: ");
+        int num = in.nextInt();
         // and search magicHand here
         //Then report the result here
     }
