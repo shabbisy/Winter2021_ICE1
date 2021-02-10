16ff0c9e4ce6654ce0ced7cc81689fb6dcde3588
Akshita <Akshita@DESKTOP-VQDS947>
2021-02-10 10:43:56 -0800

    New scanner introduced. Akshita Sreenivasulu February 8, 2020 991592661

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -31,6 +31,7 @@ public class CardTrick {
             magicHand[i] = c;
         }
 
+        Scanner in = new Scanner(System.in);
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
